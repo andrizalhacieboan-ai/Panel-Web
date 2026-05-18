2f23009c47de6ff0e5d793a8567938e646de2034
ANDRI-STORE <andrizalhacieboan@gmail.com>
2026-05-18 14:54:14 +0700

    Fix import path for CartContext in CartDrawer

--- src/components/CartDrawer.jsx
+++ src/components/CartDrawer.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { CartContext } from 'CartContext';
+import { CartContext } from './CartContext';
 import { FaTimes, FaTrash } from 'react-icons/fa';
 
 export default function CartDrawer({ onCheckoutItem }) {
